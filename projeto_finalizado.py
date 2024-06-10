82f14b2d079ef6c12c48b78a6b7ce63b0f5bc08f
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2024-06-10 14:22:23 -0300

    att

--- projeto_finalizado.py
+++ projeto_finalizado.py
@@ -135,7 +135,7 @@ def pagina_principal():
 def main():
     inicializacao()
     pagina_principal()
-    tab1, tab2 = st.sidebar.tabs(['Conversas'])
+    tab1 = st.sidebar.tabs(['Conversas'])
     tab_conversas(tab1)
     tab_configuracoes(tab2)
     
