3510084a6143dbc4325da21d363132eee484a2d1
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2024-06-11 10:01:59 -0300

    att

--- projeto_finalizado.py
+++ projeto_finalizado.py
@@ -9,7 +9,7 @@ from utils_openai import retorna_resposta_modelo
 from utils_files import *
 
 st.write("API_KEY:", st.secrets["API_KEY"])
-st.secrets["API_KEY"]
+chave = st.secrets["API_KEY"]
 
 # INICIALIZAÇÃO ==================================================
 def inicializacao():
